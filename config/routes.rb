f727e8f366b48d100c401499cade6f08f318a37e
Jean Pier <juanpedrormm92@gmail.com>
2020-11-02 21:37:36 -0300

    tercer commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/one'
-  get 'pages/two'
-
   root 'pages#one'
+  get 'pages/two'
+  get 'pages/three'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
